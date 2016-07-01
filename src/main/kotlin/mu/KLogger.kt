45b491fd2760c5b6b5c17b72f9522862fd49cb48
oshai <ohadshai@gmail.com>
2016-07-02 00:01:26 +0300

    fix compilation error, removed not needed Unit

--- src/main/kotlin/mu/KLogger.kt
+++ src/main/kotlin/mu/KLogger.kt
@@ -5,17 +5,19 @@ import org.slf4j.Logger
 
 class KLogger(jLogger: Logger): Logger by jLogger{
 
-    fun trace(msg: () -> String): Unit {
+    fun trace(msg: () -> String) {
         if (isTraceEnabled) trace(msg.invoke())
     }
-    fun debug(msg: () -> String): Unit {
+    fun debug(msg: () -> String) {
         if (isDebugEnabled) debug(msg.invoke())
     }
-    fun info(msg: () -> String): Unit {
+    fun info(msg: () -> String) {
         if (isInfoEnabled) info(msg.invoke())
     }
-    fun warn(msg: () -> String): Unit {
+    fun warn(msg: () -> String) {
         if (isWarnEnabled) warn(msg.invoke())
     }
-    fun error(msg: () -> String): Unit = if (isErrorEnabled) error(msg.invoke())
+    fun error(msg: () -> String) {
+        if (isErrorEnabled) error(msg.invoke())
+    }
 }
