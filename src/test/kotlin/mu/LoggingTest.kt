f4cbccdb1cf15bad4b4c3ef7ba022e8e60e4aec2
oshai <ohadshai@gmail.com>
2016-10-14 01:50:47 +0300

    testing regular log messages

--- src/test/kotlin/mu/LoggingTest.kt
+++ src/test/kotlin/mu/LoggingTest.kt
@@ -1,7 +1,12 @@
 package mu
 
+import org.apache.log4j.*
+import org.junit.After
+import org.junit.Assert
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
+import java.io.StringWriter
 
 class ClassWithLogging {
     companion object: KLogging()
@@ -48,7 +53,58 @@ class ChildClassWithLogging {
     }
 }
 class LoggingTest {
+    private var appenderWithWriter = AppenderWithWriter()
 
+    @Before
+    fun setupAppender() {
+        appenderWithWriter = AppenderWithWriter()
+        Logger.getRootLogger().addAppender(appenderWithWriter.appender)
+    }
+
+    @After
+    fun removeAppender() {
+        Logger.getRootLogger().removeAppender(appenderWithWriter.appender)
+    }
+
+    @Test
+    fun testMessages1() {
+        ClassWithLogging().apply {
+            test()
+            testThrowable()
+        }
+        val lines = appenderWithWriter.writer.toString().trim().replace("\r", "\n").replace("\n\n", "\n").split("\n")
+        Assert.assertEquals("INFO  mu.ClassWithLogging  - test ClassWithLogging", lines[0].trim())
+        Assert.assertEquals("TRACE mu.ClassWithLogging  - test ChildClassWithLogging", lines[1].trim())
+        Assert.assertEquals("java.lang.Throwable", lines[2].trim())
+        Assert.assertTrue(lines[3].trim().startsWith("at mu.ClassWithLogging.testThrowable("))
+    }
+    @Test
+    fun testMessages2() {
+        ClassInheritLogging().test()
+        Assert.assertEquals("INFO  mu.ClassInheritLogging  - test ClassHasLogging", appenderWithWriter.writer.toString().trim())
+    }
+    @Test
+    fun testMessages3() {
+        ChildClassWithLogging().test()
+        Assert.assertEquals("INFO  mu.ChildClassWithLogging  - test ChildClassWithLogging", appenderWithWriter.writer.toString().trim())
+    }
+    @Test
+    fun testMessages4() {
+        ClassWithNamedLogging().test()
+        Assert.assertEquals("INFO  mu.ClassWithNamedLogging  - test ClassWithNamedLogging", appenderWithWriter.writer.toString().trim())
+    }
+    @Test
+    fun testMessages5() {
+        ClassHasLogging().test()
+        Assert.assertEquals("INFO  mu.ClassHasLogging  - test ClassHasLogging", appenderWithWriter.writer.toString().trim())
+    }
+    @Test
+    fun testMessages6() {
+        CompanionHasLogging().test()
+        Assert.assertEquals("INFO  mu.CompanionHasLogging  - test CompanionHasLogging", appenderWithWriter.writer.toString().trim())
+    }
+}
+class LoggingNameTest {
     @Test
     fun testNames() {
         assertEquals("mu.ClassWithLogging", ClassWithLogging.logger.name)
@@ -58,18 +114,7 @@ class LoggingTest {
         assertEquals("mu.ClassHasLogging", ClassHasLogging().logger.name)
         assertEquals("mu.CompanionHasLogging", CompanionHasLogging.logger.name)
     }
-
-    @Test
-    fun testMessages() {
-        ClassWithLogging().apply {
-            test()
-            testThrowable()
-        }
-        ClassInheritLogging().test()
-        ChildClassWithLogging().test()
-        ClassWithNamedLogging().test()
-        ClassHasLogging().test()
-        CompanionHasLogging().test()
-    }
+}
+data class AppenderWithWriter(val writer: StringWriter = StringWriter(), val appender: Appender =  WriterAppender(PatternLayout("%-5p %c %x - %m%n"), writer)) {
 
 }
