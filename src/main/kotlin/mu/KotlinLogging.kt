f59c515fff3aca5f4c97e9314cba44f681013b36
oshai <ohadshai@gmail.com>
2016-12-14 23:09:02 +0200

    fix typo

--- src/main/kotlin/mu/KotlinLogging.kt
+++ src/main/kotlin/mu/KotlinLogging.kt
@@ -5,7 +5,7 @@ import mu.internal.KLoggerFactory
 
 object KotlinLogging {
     /**
-     * This methods allow defining the logger in a file in the following way:
+     * This method allow defining the logger in a file in the following way:
      * val logger = KotlinLogging.logger {}
      */
     fun logger(func: () -> Unit): KLogger = KLoggerFactory.logger(func)
