bca05918a3203cba64b8bed77d51c6c29de748b9
oshai <ohadshai@gmail.com>
2017-03-01 20:51:42 +0200

    added a fix for issue #17
    implemented all log methods in location aware logger

--- src/main/kotlin/mu/internal/LocationAwareKLogger.kt
+++ src/main/kotlin/mu/internal/LocationAwareKLogger.kt
@@ -474,4 +474,73 @@ internal class LocationAwareKLogger(private val jLogger: LocationAwareLogger) : 
                 LocationAwareLogger.ERROR_INT, msg, null, t)
     }
 
+    /**
+     * Lazy add a log message if isTraceEnabled is true
+     */
+    override fun trace(msg: () -> Any?) {
+        if (isTraceEnabled) trace(msg.invoke().toString())
+    }
+
+    /**
+     * Lazy add a log message if isDebugEnabled is true
+     */
+    override fun debug(msg: () -> Any?) {
+        if (isDebugEnabled) debug(msg.invoke().toString())
+    }
+
+    /**
+     * Lazy add a log message if isInfoEnabled is true
+     */
+    override fun info(msg: () -> Any?) {
+        if (isInfoEnabled) info(msg.invoke().toString())
+    }
+
+    /**
+     * Lazy add a log message if isWarnEnabled is true
+     */
+    override fun warn(msg: () -> Any?) {
+        if (isWarnEnabled) warn(msg.invoke().toString())
+    }
+
+    /**
+     * Lazy add a log message if isErrorEnabled is true
+     */
+    override fun error(msg: () -> Any?) {
+        if (isErrorEnabled) error(msg.invoke().toString())
+    }
+
+    /**
+     * Lazy add a log message with throwable payload if isTraceEnabled is true
+     */
+    override fun trace(t: Throwable, msg: () -> Any?) {
+        if (isTraceEnabled) trace(msg.invoke().toString(), t)
+    }
+
+    /**
+     * Lazy add a log message with throwable payload if isDebugEnabled is true
+     */
+    override fun debug(t: Throwable, msg: () -> Any?) {
+        if (isDebugEnabled) debug(msg.invoke().toString(), t)
+    }
+
+    /**
+     * Lazy add a log message with throwable payload if isInfoEnabled is true
+     */
+    override fun info(t: Throwable, msg: () -> Any?) {
+        if (isInfoEnabled) info(msg.invoke().toString(), t)
+    }
+
+    /**
+     * Lazy add a log message with throwable payload if isWarnEnabled is true
+     */
+    override fun warn(t: Throwable, msg: () -> Any?) {
+        if (isWarnEnabled) warn(msg.invoke().toString(), t)
+    }
+
+    /**
+     * Lazy add a log message with throwable payload if isErrorEnabled is true
+     */
+    override fun error(t: Throwable, msg: () -> Any?) {
+        if (isErrorEnabled) error(msg.invoke().toString(), t)
+    }
 }
