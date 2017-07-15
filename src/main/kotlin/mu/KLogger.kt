5308c2773c10de7b19714739d16a0350819deb34
oshai <ohadshai@gmail.com>
2017-07-15 20:52:43 +0300

    change documentation

--- src/main/kotlin/mu/KLogger.kt
+++ src/main/kotlin/mu/KLogger.kt
@@ -11,7 +11,7 @@ import org.slf4j.Logger
 interface KLogger: Logger{
 
     /**
-     * The actual logger execution is delegated into
+     * The actual logger executing logging
      */
     val underlyingLogger: Logger
 
