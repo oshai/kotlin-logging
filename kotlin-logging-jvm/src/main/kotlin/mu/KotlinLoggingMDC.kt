c6dc3a8b387a1701cc5f388c46f58319c48d1d4f
oshai <oshai@outbrain.com>
2017-12-31 10:41:14 +0200

    add documentation

--- kotlin-logging-jvm/src/main/kotlin/mu/KotlinLoggingMDC.kt
+++ kotlin-logging-jvm/src/main/kotlin/mu/KotlinLoggingMDC.kt
@@ -2,9 +2,21 @@ package mu
 
 import org.slf4j.MDC
 
+/**
+ * Use a pair in MDC context. Example:
+ * `withLoggingContext("userId" to userId) {
+ *   doSomething()
+ * }`
+ */
 inline fun <T> withLoggingContext(pair: Pair<String, String>, body: () -> T): T =
     MDC.putCloseable(pair.first, pair.second).use { body() }
 
+/**
+ * Use a vary number of pairs in MDC context. Example:
+ * `withLoggingContext("userId" to userId) {
+ *   doSomething()
+ * }`
+ */
 inline fun <T> withLoggingContext(vararg pair: Pair<String, String>, body: () -> T): T {
   try {
     pair.forEach { MDC.put(it.first, it.second) }
@@ -14,6 +26,12 @@ inline fun <T> withLoggingContext(vararg pair: Pair<String, String>, body: () ->
   }
 }
 
+/**
+ * Use a map in MDC context. Example:
+ * `withLoggingContext(mapOf("userId" to userId)) {
+ *   doSomething()
+ * }`
+ */
 inline fun <T> withLoggingContext(map: Map<String, String>, body: () -> T): T {
   try {
     map.forEach { MDC.put(it.key, it.value) }
