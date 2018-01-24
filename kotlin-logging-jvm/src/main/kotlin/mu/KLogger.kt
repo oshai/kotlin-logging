222290dbf229678bbe0685f35af60d3fb3c59570
oshai <ohadshai@gmail.com>
2018-01-24 10:43:33 +0100

    Multiplatform (#35)
    * issue #21 - make the lib multiplatform
    - split to common/jvm/js
    - left all code in jvm at the moment
    - add build scripts
    
    * issue #31 - Add MDC support: withLoggingContext
    
    * merge to branch: issue #31 - Add MDC support: withLoggingContext
    
    * add documentation
    
    * rebase (#33)
    
    * issue #31 - Add MDC support: withLoggingContext
    
    * Update ChangleLog.md
    
    * Update ChangleLog.md
    
    * Update README.md
    
    * Rename ChangleLog.md to ChangeLog.md (#32)
    
    * update to kotlin 1.2.20
    
    * split packages and gradle build
    
    * split packages and gradle build
    
    * split packages and gradle build
    
    * fix src root for jvm project
    
    * fix artifact build for jvm version

---
+++ kotlin-logging-jvm/src/main/kotlin/mu/KLogger.kt
@@ -0,0 +1,87 @@
+package mu
+
+import mu.internal.toStringSafe
+import org.slf4j.Logger
+
+/**
+ * An extension for [Logger] with Lazy message evaluation
+ * example:
+ * logger.info{"this is $lazy evaluated string"}
+ */
+interface KLogger : Logger {
+
+  /**
+   * The actual logger executing logging
+   */
+  val underlyingLogger: Logger
+
+  /**
+   * Lazy add a log message if isTraceEnabled is true
+   */
+  fun trace(msg: () -> Any?) {
+    if (isTraceEnabled) trace(msg.toStringSafe())
+  }
+
+  /**
+   * Lazy add a log message if isDebugEnabled is true
+   */
+  fun debug(msg: () -> Any?) {
+    if (isDebugEnabled) debug(msg.toStringSafe())
+  }
+
+  /**
+   * Lazy add a log message if isInfoEnabled is true
+   */
+  fun info(msg: () -> Any?) {
+    if (isInfoEnabled) info(msg.toStringSafe())
+  }
+
+  /**
+   * Lazy add a log message if isWarnEnabled is true
+   */
+  fun warn(msg: () -> Any?) {
+    if (isWarnEnabled) warn(msg.toStringSafe())
+  }
+
+  /**
+   * Lazy add a log message if isErrorEnabled is true
+   */
+  fun error(msg: () -> Any?) {
+    if (isErrorEnabled) error(msg.toStringSafe())
+  }
+
+  /**
+   * Lazy add a log message with throwable payload if isTraceEnabled is true
+   */
+  fun trace(t: Throwable, msg: () -> Any?) {
+    if (isTraceEnabled) trace(msg.toStringSafe(), t)
+  }
+
+  /**
+   * Lazy add a log message with throwable payload if isDebugEnabled is true
+   */
+  fun debug(t: Throwable, msg: () -> Any?) {
+    if (isDebugEnabled) debug(msg.toStringSafe(), t)
+  }
+
+  /**
+   * Lazy add a log message with throwable payload if isInfoEnabled is true
+   */
+  fun info(t: Throwable, msg: () -> Any?) {
+    if (isInfoEnabled) info(msg.toStringSafe(), t)
+  }
+
+  /**
+   * Lazy add a log message with throwable payload if isWarnEnabled is true
+   */
+  fun warn(t: Throwable, msg: () -> Any?) {
+    if (isWarnEnabled) warn(msg.toStringSafe(), t)
+  }
+
+  /**
+   * Lazy add a log message with throwable payload if isErrorEnabled is true
+   */
+  fun error(t: Throwable, msg: () -> Any?) {
+    if (isErrorEnabled) error(msg.toStringSafe(), t)
+  }
+}
---
+++ kotlin-logging-jvm/src/main/kotlin/mu/KotlinLoggingMDC.kt
@@ -0,0 +1,42 @@
+package mu
+
+import org.slf4j.MDC
+
+/**
+ * Use a pair in MDC context. Example:
+ * `withLoggingContext("userId" to userId) {
+ *   doSomething()
+ * }`
+ */
+inline fun <T> withLoggingContext(pair: Pair<String, String>, body: () -> T): T =
+    MDC.putCloseable(pair.first, pair.second).use { body() }
+
+/**
+ * Use a vary number of pairs in MDC context. Example:
+ * `withLoggingContext("userId" to userId) {
+ *   doSomething()
+ * }`
+ */
+inline fun <T> withLoggingContext(vararg pair: Pair<String, String>, body: () -> T): T {
+  try {
+    pair.forEach { MDC.put(it.first, it.second) }
+    return body()
+  } finally {
+    pair.forEach { MDC.remove(it.first) }
+  }
+}
+
+/**
+ * Use a map in MDC context. Example:
+ * `withLoggingContext(mapOf("userId" to userId)) {
+ *   doSomething()
+ * }`
+ */
+inline fun <T> withLoggingContext(map: Map<String, String>, body: () -> T): T {
+  try {
+    map.forEach { MDC.put(it.key, it.value) }
+    return body()
+  } finally {
+    map.forEach { MDC.remove(it.key) }
+  }
+}
