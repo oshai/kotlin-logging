12cd9f7b5c9165436dcab0d1a27f4b44d2ac90d7
oshai <ohadshai@gmail.com>
2018-08-01 01:07:42 +0300

    add test, in current version couldn't still run it checked it in another class with version 1.2.51 and it was green

---
+++ kotlin-logging-common/src/test/kotlin/mu/internal/MessageInvokerTest.kt
@@ -0,0 +1,15 @@
+package mu.internal
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+
+class MessageInvokerTest {
+
+    @Test
+    fun `toStringSafe checks`() {
+       assertEquals(Unit.toString(), {} .toStringSafe())
+       assertEquals("null", {null} .toStringSafe())
+       assertEquals("hi", {"hi"} .toStringSafe())
+    }
+}
