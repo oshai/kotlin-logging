da955dd161453b847022321d5ac78bd46f5b23ee
Rob Murdock <rmurdock@pillartechnology.com>
2019-02-13 19:05:00 -0500

    Loosening up the interface even more - its imaginable that some people logging in javascript would want to pass an object reference to the logger rather than a string.

--- kotlin-logging-js/src/main/kotlin/mu/MessageFormatter.kt
+++ kotlin-logging-js/src/main/kotlin/mu/MessageFormatter.kt
@@ -1,8 +1,8 @@
 package mu
 
 interface MessageFormatter {
-    fun formatMessage(level: KotlinLoggingLevel, loggerName: String, msg: () -> Any?): String
-    fun formatMessage(level: KotlinLoggingLevel, loggerName: String, t: Throwable?, msg: () -> Any?): String
-    fun formatMessage(level: KotlinLoggingLevel, loggerName: String, marker: Marker?, msg: () -> Any?): String
-    fun formatMessage(level: KotlinLoggingLevel, loggerName: String, marker: Marker?, t: Throwable?, msg: () -> Any?): String
+    fun formatMessage(level: KotlinLoggingLevel, loggerName: String, msg: () -> Any?): Any?
+    fun formatMessage(level: KotlinLoggingLevel, loggerName: String, t: Throwable?, msg: () -> Any?): Any?
+    fun formatMessage(level: KotlinLoggingLevel, loggerName: String, marker: Marker?, msg: () -> Any?): Any?
+    fun formatMessage(level: KotlinLoggingLevel, loggerName: String, marker: Marker?, t: Throwable?, msg: () -> Any?): Any?
 }
