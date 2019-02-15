9a49a93a3260bdc58b34331adb2bec8f87e06c8c
oshai <ohadshai@gmail.com>
2019-02-15 10:14:19 +0200

    change names of appender and formatter to comply with java logging

--- kotlin-logging-js/src/main/kotlin/mu/KotlinLoggingConfiguration.kt
+++ kotlin-logging-js/src/main/kotlin/mu/KotlinLoggingConfiguration.kt
@@ -2,7 +2,7 @@ package mu
 
 object KotlinLoggingConfiguration {
     var LOG_LEVEL = KotlinLoggingLevel.INFO
-    var OUTPUT_PIPES: OutputPipes = ConsoleOutputPipes
-    var MESSAGE_FORMATTER: MessageFormatter = DefaultMessageFormatter
+    var APPENDER: OutputPipes = ConsoleOutputPipes
+    var FORMATTER: MessageFormatter = DefaultMessageFormatter
 
 }
