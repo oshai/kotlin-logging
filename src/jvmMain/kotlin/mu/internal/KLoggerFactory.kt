86d5bda5ebef38558c04ffd22548d4296dedf391
oshai <ohadshai@gmail.com>
2019-08-15 09:39:44 +0300

    fix warning

--- src/jvmMain/kotlin/mu/internal/KLoggerFactory.kt
+++ src/jvmMain/kotlin/mu/internal/KLoggerFactory.kt
@@ -15,29 +15,29 @@ internal object KLoggerFactory {
     /**
      * get logger for the class
      */
-    inline internal fun logger(loggable: KLoggable): KLogger =
+    internal inline fun logger(loggable: KLoggable): KLogger =
         logger(KLoggerNameResolver.name(loggable.javaClass))
 
     /**
      * get logger by explicit name
      */
-    inline internal fun logger(name: String): KLogger = wrapJLogger(jLogger(name))
+    internal inline fun logger(name: String): KLogger = wrapJLogger(jLogger(name))
 
     /**
      * get logger for the method, assuming it was declared at the logger file/class
      */
-    inline internal fun logger(noinline func: () -> Unit): KLogger =
+    internal inline fun logger(noinline func: () -> Unit): KLogger =
         logger(KLoggerNameResolver.name(func))
 
     /**
      * get a java logger by name
      */
-    inline private fun jLogger(name: String): Logger = LoggerFactory.getLogger(name)
+    private inline fun jLogger(name: String): Logger = LoggerFactory.getLogger(name)
 
     /**
      * wrap java logger based on location awareness
      */
-    inline private fun wrapJLogger(jLogger: Logger): KLogger =
+    private inline fun wrapJLogger(jLogger: Logger): KLogger =
         if (jLogger is LocationAwareLogger)
             LocationAwareKLogger(jLogger)
         else
