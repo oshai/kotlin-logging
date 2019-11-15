713a129fabe882b7a96e2ad9706371058e49fc09
Piyush Mor <piyush.mor@zalando.de>
2019-11-15 23:41:36 +0100

    use Dokka for javadocs #82
    Signed-off-by: Piyush Mor <piyush.mor@zalando.de>

--- src/jsMain/kotlin/mu/KotlinLogging.kt
+++ src/jsMain/kotlin/mu/KotlinLogging.kt
@@ -6,7 +6,9 @@ import mu.internal.KLoggerJS
 actual object KotlinLogging {
     /**
      * This method allow defining the logger in a file in the following way:
+     * ```
      * val logger = KotlinLogging.logger {}
+     * ```
      */
     actual fun logger(func: () -> Unit): KLogger = KLoggerJS(func::class.js.name)
 
