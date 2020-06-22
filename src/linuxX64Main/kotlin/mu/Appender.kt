16d190b24d6fe7e7f2c701f5e2f6c89fc7173368
Nick Apperley <napperley@gmail.com>
2020-06-22 14:26:14 +1200

    Created Appender interface in common module.

--- src/linuxX64Main/kotlin/mu/Appender.kt
+++
@@ -1,9 +0,0 @@
-package mu
-
-interface Appender {
-    fun trace(message: Any?)
-    fun debug(message: Any?)
-    fun info(message: Any?)
-    fun warn(message: Any?)
-    fun error(message: Any?)
-}
