e9d90046caecb08a29fceb4bcfa26057537cde18
Alexander Nozik <altavir@gmail.com>
2020-09-09 12:48:30 +0300

    Deploy to bintray

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,39 +1,21 @@
-import java.util.*
-
 plugins {
     kotlin("multiplatform") version "1.4.0"
-    id("com.jfrog.bintray") version "1.8.4"
-    id("org.jetbrains.dokka") version "1.4.0-rc"
+//    id("com.jfrog.bintray") version "1.8.4"
+    id("org.jetbrains.dokka") version "1.4.0"
     `maven-publish`
-    `java-library`
+    id("ru.mipt.npm.publish")
 }
 
-buildscript {
-    apply("versions.gradle.kts")
-}
+
+apply("versions.gradle.kts")
+
 
 group = "io.github.microutils"
 version = "1.9.0-dev-npm"
 
 repositories {
-    mavenCentral()
     jcenter()
-}
-
-tasks {
-    dokkaHtml.configure {
-        dokkaSourceSets {
-            register("commonMain") {
-                displayName = "common"
-                platform = "common"
-            }
-        }
-    }
-    register<Jar>("dokkaJar") {
-        from(dokkaHtml)
-        dependsOn(dokkaHtml)
-        archiveClassifier.set("javadoc")
-    }
+    mavenCentral()
 }
 
 kotlin {
@@ -56,7 +38,10 @@ kotlin {
     val nativeTarget = when {
         hostOs == "Mac OS X" -> macosX64("native")
         hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
+        isMingwX64 -> {
+            mingwX64("native")
+            linuxX64()
+        }
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
     sourceSets {
@@ -89,6 +74,16 @@ kotlin {
                 implementation(kotlin("test-js"))
             }
         }
+        val nativeMain by getting
+        findByName("linuxX64Main")?.dependsOn(nativeMain)
+    }
+}
+
+tasks {
+    register<Jar>("dokkaJar") {
+        from(dokkaHtml)
+        dependsOn(dokkaHtml)
+        archiveClassifier.set("javadoc")
     }
 }
 
@@ -122,57 +117,70 @@ publishing {
     }
 }
 
-bintray {
-    user = "oshai"//project.hasProperty("bintrayUser") ? project.property("bintrayUser") : System.getenv("BINTRAY_USER")
-    key = "mykey" //https://bintray.com/profile/edit
-    // project.hasProperty("bintrayApiKey") ? project.property("bintrayApiKey") : System.getenv("BINTRAY_API_KEY")
-    setPublications("metadata", "jvm", "js")
-    publish = true //[Default: false] Whether version should be auto published after an upload
-    pkg.apply {
-        repo = "kotlin-logging"
-        name = "kotlin-logging"
-        userOrg = "microutils"
-        setLicenses("Apache-2.0")
-        vcsUrl = "https://github.com/MicroUtils/kotlin-logging"
-        websiteUrl = "https://github.com/MicroUtils/kotlin-logging"
-        issueTrackerUrl = "https://github.com/MicroUtils/kotlin-logging/issues"
+//bintray {
+//    user = "oshai"//project.hasProperty("bintrayUser") ? project.property("bintrayUser") : System.getenv("BINTRAY_USER")
+//    key = "mykey" //https://bintray.com/profile/edit
+//    // project.hasProperty("bintrayApiKey") ? project.property("bintrayApiKey") : System.getenv("BINTRAY_API_KEY")
+//    setPublications("metadata", "jvm", "js")
+//    publish = true //[Default: false] Whether version should be auto published after an upload
+//    pkg.apply {
+//        repo = "kotlin-logging"
+//        name = "kotlin-logging"
+//        userOrg = "microutils"
+//        setLicenses("Apache-2.0")
+//        vcsUrl = "https://github.com/MicroUtils/kotlin-logging"
+//        websiteUrl = "https://github.com/MicroUtils/kotlin-logging"
+//        issueTrackerUrl = "https://github.com/MicroUtils/kotlin-logging/issues"
+//
+//        githubRepo = "MicroUtils/kotlin-logging"
+//        githubReleaseNotesFile = "ChangeLog.md"
+//        version.apply {
+//            name = "${project.version}"
+//            desc = "kotlin-logging - Lightweight logging framework for Kotlin"
+//            val date = Date()
+//            released = "$date"
+//            gpg.sign = true //Determines whether to GPG sign the files. The default is false
+//            mavenCentralSync.apply {
+//                sync = true //[Default: true] Determines whether to sync the version to Maven Central.
+//                user = "token" //OSS user token: mandatory
+//                password = "pass" //OSS user password: mandatory
+//                close = "1" //Optional property. By default the staging repository is closed and artifacts are released to Maven Central. You can optionally turn this behaviour off (by puting 0 as value) and release the version manually.
+//            }
+//        }
+//    }
+//}
 
-        githubRepo = "MicroUtils/kotlin-logging"
-        githubReleaseNotesFile = "ChangeLog.md"
-        version.apply {
-            name = "${project.version}"
-            desc = "kotlin-logging - Lightweight logging framework for Kotlin"
-            released = "${Date()}"
-            gpg.sign = true //Determines whether to GPG sign the files. The default is false
-            mavenCentralSync.apply {
-                sync = true //[Default: true] Determines whether to sync the version to Maven Central.
-                user = "token" //OSS user token: mandatory
-                password = "pass" //OSS user password: mandatory
-                close = "1" //Optional property. By default the staging repository is closed and artifacts are released to Maven Central. You can optionally turn this behaviour off (by puting 0 as value) and release the version manually.
-            }
-        }
+
+/* Kscience publications */
+kscience{
+    publish {
+        bintrayRepo = "dev"
+        githubOrg = "altavir"
+        githubProject = "kotlin-logging"
     }
 }
 
-/* Publication for github packages */
-
-val githubUser: String? by project
-val githubToken: String? by project
-
-afterEvaluate {
-    project.configure<PublishingExtension> {
-        if (githubUser != null && githubToken != null) {
-            repositories {
-                maven {
-                    name = "github"
-                    url = uri("https://maven.pkg.github.com/altavir/kotlin-logging/")
-                    credentials {
-                        username = githubUser
-                        password = githubToken
-                    }
-
-                }
-            }
-        }
-    }
-}
+//val githubUser: String? by project
+//val githubToken: String? by project
+//
+//val bintrayRepo by extra("dev")
+//
+//val githubProject by extra("dataforge-core")
+//
+//afterEvaluate {
+//    project.configure<PublishingExtension> {
+//        if (githubUser != null && githubToken != null) {
+//            repositories {
+//                maven {
+//                    name = "github"
+//                    url = uri("https://maven.pkg.github.com/altavir/kotlin-logging/")
+//                    credentials {
+//                        username = githubUser
+//                        password = githubToken
+//                    }
+//
+//                }
+//            }
+//        }
+//    }
+//}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,17 @@
-rootProject.name = "kotlin-logging"
-
 pluginManagement {
     repositories {
+        mavenLocal()
         gradlePluginPortal()
         jcenter()
+        maven("https://dl.bintray.com/mipt-npm/kscience")
+        maven("https://dl.bintray.com/mipt-npm/dev")
+    }
+
+    val toolsVersion = "0.6.0-dev-6"
+
+    plugins {
+        id("ru.mipt.npm.publish") version toolsVersion
     }
 }
+
+rootProject.name = "kotlin-logging"
