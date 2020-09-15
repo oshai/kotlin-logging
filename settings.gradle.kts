a71ab72d73163cea361d8020241d116b87451d40
Alexander Nozik <altavir@gmail.com>
2020-09-15 14:37:43 +0300

    Clear the remnants of npm plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,7 @@
 pluginManagement {
     repositories {
-        mavenLocal()
         gradlePluginPortal()
         jcenter()
-        maven("https://dl.bintray.com/mipt-npm/kscience")
-        maven("https://dl.bintray.com/mipt-npm/dev")
-    }
-
-    val toolsVersion = "0.6.0"
-
-    plugins {
-        id("ru.mipt.npm.publish") version toolsVersion
     }
 }
 
