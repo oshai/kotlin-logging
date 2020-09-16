fe520e12e00263f64b5a6cce39e3012c3f70a858
Ohad Shai <oshai@google.com>
2020-09-16 12:29:16 +0300

    Add support for windows and multiple os
    will build on windows,linux and mac.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 apply("versions.gradle.kts")
 
 group = "io.github.microutils"
-version = "2.0.0" + (if (System.getProperty("snapshot")?.toBoolean() == true) "-SNAPSHOT" else "")
+version = "2.0.0-win" + (if (System.getProperty("snapshot")?.toBoolean() == true) "-SNAPSHOT" else "")
 
 repositories {
     jcenter()
