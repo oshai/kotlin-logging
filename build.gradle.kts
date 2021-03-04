c59082a8a34704b69273cfe20b3f516d68923732
Ohad Shai <oshai@google.com>
2021-03-04 21:53:31 +0200

    update kotlin version to 1.4.31

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.4.21"
+    kotlin("multiplatform") version "1.4.31"
     id("org.jetbrains.dokka") version "1.4.0"
     id("com.jfrog.artifactory") version "4.17.2"
     `maven-publish`
