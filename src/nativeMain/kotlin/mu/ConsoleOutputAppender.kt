8b0fae17ec4693c53981e66c42539b6d066d7435
Conrad Kramer <conrad@conradkramer.com>
2022-04-08 11:35:04 -0700

    Add loggerName to the Appender interface
    On Darwin platforms using os_log, a separate logger can be created for
    each differently named logger. The Appender, which directs the text of
    the log message, needs to know which logger to direct it to.

--- src/nativeMain/kotlin/mu/ConsoleOutputAppender.kt
+++ src/nativeMain/kotlin/mu/ConsoleOutputAppender.kt
@@ -4,12 +4,12 @@ import platform.posix.fprintf
 import platform.posix.stderr
 
 public object ConsoleOutputAppender : Appender {
-    public override fun trace(message: Any?): Unit = println(message)
-    public override fun debug(message: Any?): Unit = println(message)
-    public override fun info(message: Any?): Unit = println(message)
-    public override fun warn(message: Any?): Unit = println(message)
+    public override fun trace(loggerName: String, message: String): Unit = println(message)
+    public override fun debug(loggerName: String, message: String): Unit = println(message)
+    public override fun info(loggerName: String, message: String): Unit = println(message)
+    public override fun warn(loggerName: String, message: String): Unit = println(message)
 
-    override fun error(message: Any?) {
+    override fun error(loggerName: String, message: String) {
         fprintf(stderr, "$message\n")
     }
 }
