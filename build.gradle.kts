9a01abbd3bc01825390366d61be8429dce9bc275
Yeikel <yeikel@users.noreply.github.com>
2022-10-05 11:36:34 -0400

    build(deps): bump kotlin multiplatform from 1.6.0 to 1.7.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.jvm.tasks.Jar
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
 
 plugins {
-    kotlin("multiplatform") version "1.6.0"
+    kotlin("multiplatform") version "1.7.20"
     id("org.jetbrains.dokka") version "1.6.0"
     `maven-publish`
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
