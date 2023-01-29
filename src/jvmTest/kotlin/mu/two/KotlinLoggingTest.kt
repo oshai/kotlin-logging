db615e37d59507eefc434f0608ada0a5d243fce6
oshai <ohadshai@gmail.com>
2023-01-29 21:13:21 +0200

    fix compilation

--- src/jvmTest/kotlin/mu/two/KotlinLoggingTest.kt
+++ src/jvmTest/kotlin/mu/two/KotlinLoggingTest.kt
@@ -1,5 +1,7 @@
 package mu.two
 
+import mu.two.slf4j.logger
+import mu.two.slf4j.toKLogger
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
