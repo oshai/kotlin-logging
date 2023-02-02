0a406e565a6f0b393be436b0c56981c8d634ec17
oshai <ohadshai@gmail.com>
2023-02-02 19:37:42 +0200

    trying a different repo url

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ nexusPublishing {
     repositories {
         sonatype {  //only for users registered in Sonatype after 24 Feb 2021
             nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
         }
     }
 }
