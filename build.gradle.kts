44280787e98199b31a16abd9c2739aae4e7ef5a3
oshai <ohadshai@gmail.com>
2023-02-02 16:03:55 +0200

    change maven url

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,10 @@ repositories {
 
 nexusPublishing {
     repositories {
-        sonatype()
+        sonatype {  //only for users registered in Sonatype after 24 Feb 2021
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        }
     }
 }
 
