e89c84110a1519a1a879f8a4726a79270d2b84c8
oshai <ohadshai@gmail.com>
2023-02-11 01:01:33 +0200

    docs and formatting

--- src/nativeMain/kotlin/io/github/oshai/internal/KLoggerLinux.kt
+++ src/nativeMain/kotlin/io/github/oshai/internal/KLoggerLinux.kt
@@ -120,9 +120,38 @@ internal class KLoggerLinux(override val name: String) : KLogger {
     ERROR.logIfEnabled({ "catching($throwable" }, throwable, appender::error)
   }
 
+  /**
+   * Is the logger instance enabled for the TRACE level?
+   *
+   * @return True if this Logger is enabled for the TRACE level, false otherwise.
+   */
   override val isTraceEnabled: Boolean = TRACE.isLoggingEnabled()
+
+  /**
+   * Is the logger instance enabled for the DEBUG level?
+   *
+   * @return True if this Logger is enabled for the DEBUG level, false otherwise.
+   */
   override val isDebugEnabled: Boolean = DEBUG.isLoggingEnabled()
+
+  /**
+   * Is the logger instance enabled for the INFO level?
+   *
+   * @return True if this Logger is enabled for the INFO level, false otherwise.
+   */
   override val isInfoEnabled: Boolean = INFO.isLoggingEnabled()
+
+  /**
+   * Is the logger instance enabled for the WARN level?
+   *
+   * @return True if this Logger is enabled for the WARN level, false otherwise.
+   */
   override val isWarnEnabled: Boolean = WARN.isLoggingEnabled()
+
+  /**
+   * Is the logger instance enabled for the ERROR level?
+   *
+   * @return True if this Logger is enabled for the ERROR level, false otherwise.
+   */
   override val isErrorEnabled: Boolean = ERROR.isLoggingEnabled()
 }
