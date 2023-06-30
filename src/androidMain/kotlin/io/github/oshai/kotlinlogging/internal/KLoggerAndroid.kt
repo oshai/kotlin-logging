cc4e2941f8d0ac1d54f6868d6674301bc39057a2
oshai <ohadshai@gmail.com>
2023-06-30 11:19:59 +0300

    implement more methods.

--- src/androidMain/kotlin/io/github/oshai/kotlinlogging/internal/KLoggerAndroid.kt
+++ src/androidMain/kotlin/io/github/oshai/kotlinlogging/internal/KLoggerAndroid.kt
@@ -7,46 +7,42 @@ import io.github.oshai.kotlinlogging.Marker
 @Suppress("TooManyFunctions")
 internal class KLoggerAndroid(override val name: String) : KLogger {
   override val underlyingLogger: Any
-    get() = TODO("Not yet implemented")
+    get() = TODO("Not implemented")
 
-  override fun trace(msg: String?) {
-    TODO("Not yet implemented")
-  }
+  override fun trace(msg: String?) = trace { msg }
 
   override fun trace(msg: String?, arg: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun trace(msg: String?, arg1: Any?, arg2: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun trace(msg: String?, vararg arguments: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
-  override fun trace(msg: String?, t: Throwable?) {
-    TODO("Not yet implemented")
-  }
+  override fun trace(msg: String?, t: Throwable?) = trace(t) { msg }
 
   override fun trace(marker: Marker?, msg: String?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun trace(marker: Marker?, msg: String?, arg: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun trace(marker: Marker?, msg: String?, arg1: Any?, arg2: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun trace(marker: Marker?, msg: String?, vararg arguments: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun trace(marker: Marker?, msg: String?, t: Throwable?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun trace(msg: () -> Any?) {
@@ -111,193 +107,165 @@ internal class KLoggerAndroid(override val name: String) : KLogger {
   override fun error(marker: Marker?, msg: () -> Any?) = error(msg)
 
   override fun trace(marker: Marker?, t: Throwable?, msg: () -> Any?) = trace(t, msg)
-  override fun isTraceEnabled(marker: Marker?): Boolean {
-    TODO("Not yet implemented")
-  }
+  override fun isTraceEnabled(marker: Marker?): Boolean = isTraceEnabled
 
-  override fun debug(msg: String?) {
-    TODO("Not yet implemented")
-  }
+  override fun debug(msg: String?) = debug { msg }
 
   override fun debug(msg: String?, arg: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun debug(msg: String?, arg1: Any?, arg2: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun debug(msg: String?, vararg arguments: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
-  override fun debug(msg: String?, t: Throwable?) {
-    TODO("Not yet implemented")
-  }
+  override fun debug(msg: String?, t: Throwable?) = debug(t) { msg }
 
   override fun debug(marker: Marker?, msg: String?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun debug(marker: Marker?, msg: String?, arg: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun debug(marker: Marker?, msg: String?, arg1: Any?, arg2: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun debug(marker: Marker?, msg: String?, vararg arguments: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun debug(marker: Marker?, msg: String?, t: Throwable?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun debug(marker: Marker?, t: Throwable?, msg: () -> Any?) = debug(t, msg)
-  override fun isDebugEnabled(marker: Marker?): Boolean {
-    TODO("Not yet implemented")
-  }
+  override fun isDebugEnabled(marker: Marker?): Boolean = isDebugEnabled
 
-  override fun info(msg: String?) {
-    TODO("Not yet implemented")
-  }
+  override fun info(msg: String?) = info { msg }
 
   override fun info(msg: String?, arg: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun info(msg: String?, arg1: Any?, arg2: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun info(msg: String?, vararg arguments: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
-  override fun info(msg: String?, t: Throwable?) {
-    TODO("Not yet implemented")
-  }
+  override fun info(msg: String?, t: Throwable?) = info(t) { msg }
 
   override fun info(marker: Marker?, msg: String?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun info(marker: Marker?, msg: String?, arg: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun info(marker: Marker?, msg: String?, arg1: Any?, arg2: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun info(marker: Marker?, msg: String?, vararg arguments: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun info(marker: Marker?, msg: String?, t: Throwable?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun info(marker: Marker?, t: Throwable?, msg: () -> Any?) = info(t, msg)
-  override fun isInfoEnabled(marker: Marker?): Boolean {
-    TODO("Not yet implemented")
-  }
+  override fun isInfoEnabled(marker: Marker?): Boolean = isInfoEnabled
 
-  override fun warn(msg: String?) {
-    TODO("Not yet implemented")
-  }
+  override fun warn(msg: String?) = warn { msg }
 
   override fun warn(msg: String?, arg: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun warn(msg: String?, vararg arguments: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun warn(msg: String?, arg1: Any?, arg2: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
-  override fun warn(msg: String?, t: Throwable?) {
-    TODO("Not yet implemented")
-  }
+  override fun warn(msg: String?, t: Throwable?) = warn(t) { msg }
 
   override fun warn(marker: Marker?, msg: String?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun warn(marker: Marker?, msg: String?, arg: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun warn(marker: Marker?, msg: String?, arg1: Any?, arg2: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun warn(marker: Marker?, msg: String?, vararg arguments: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun warn(marker: Marker?, msg: String?, t: Throwable?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun warn(marker: Marker?, t: Throwable?, msg: () -> Any?) = warn(t, msg)
-  override fun isWarnEnabled(marker: Marker?): Boolean {
-    TODO("Not yet implemented")
-  }
+  override fun isWarnEnabled(marker: Marker?): Boolean = isWarnEnabled
 
-  override fun error(msg: String?) {
-    TODO("Not yet implemented")
-  }
+  override fun error(msg: String?) = error { msg }
 
   override fun error(msg: String?, arg: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun error(msg: String?, arg1: Any?, arg2: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun error(msg: String?, vararg arguments: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
-  override fun error(msg: String?, t: Throwable?) {
-    TODO("Not yet implemented")
-  }
+  override fun error(msg: String?, t: Throwable?) = error(t) { msg }
 
   override fun error(marker: Marker?, msg: String?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun error(marker: Marker?, msg: String?, arg: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun error(marker: Marker?, msg: String?, arg1: Any?, arg2: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun error(marker: Marker?, msg: String?, vararg arguments: Any?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun error(marker: Marker?, msg: String?, t: Throwable?) {
-    TODO("Not yet implemented")
+    TODO("Not implemented")
   }
 
   override fun error(marker: Marker?, t: Throwable?, msg: () -> Any?) = error(t, msg)
-  override fun isErrorEnabled(marker: Marker?): Boolean {
-    TODO("Not yet implemented")
-  }
+  override fun isErrorEnabled(marker: Marker?): Boolean = isErrorEnabled
 
-  override fun isLoggingOff(marker: Marker?): Boolean {
-    TODO("Not yet implemented")
-  }
+  override fun isLoggingOff(marker: Marker?): Boolean = isLoggingOff
 
   override fun entry(vararg arguments: Any?) = trace { "entry($arguments)" }
 
