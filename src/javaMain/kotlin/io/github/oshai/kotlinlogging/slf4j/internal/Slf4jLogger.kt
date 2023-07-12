84adc25a3a0651de8995a321edc79cf3fb645def
oshai <ohadshai@gmail.com>
2023-07-13 01:38:57 +0300

    formatting

--- src/javaMain/kotlin/io/github/oshai/kotlinlogging/slf4j/internal/Slf4jLogger.kt
+++ src/javaMain/kotlin/io/github/oshai/kotlinlogging/slf4j/internal/Slf4jLogger.kt
@@ -8,7 +8,7 @@ import io.github.oshai.kotlinlogging.slf4j.toSlf4j
 import org.slf4j.Logger
 
 @Suppress("DeprecatedCallableAddReplaceWith")
-public abstract class Slf4jLogger<T: Logger> : KLogger, DelegatingKLogger<T> {
+public abstract class Slf4jLogger<T : Logger> : KLogger, DelegatingKLogger<T> {
 
   // we don't move more methods to here because if it will appear on stacktrace
   // it will break fqcn for class name in location aware loggers
@@ -29,130 +29,120 @@ public abstract class Slf4jLogger<T: Logger> : KLogger, DelegatingKLogger<T> {
     }
   }
 
-    ///
-    /// The methods below are deprecated and will be removed in the future
-    ///
+  ///
+  /// The methods below are deprecated and will be removed in the future
+  ///
 
+  @Deprecated("Use trace {} instead")
+  public override fun trace(msg: String?, arg: Any?): Unit = underlyingLogger.trace(msg, arg)
 
-    @Deprecated("Use trace {} instead") public override fun trace(msg: String?, arg: Any?): Unit =
-        underlyingLogger.trace(msg, arg)
+  @Deprecated("Use trace {} instead")
+  public override fun trace(msg: String?, arg1: Any?, arg2: Any?): Unit =
+    underlyingLogger.trace(msg, arg1, arg2)
 
+  @Deprecated("Use trace {} instead")
+  public override fun trace(msg: String?, vararg arguments: Any?): Unit =
+    underlyingLogger.trace(msg, *arguments)
 
-    @Deprecated("Use trace {} instead")
-    public override fun trace(msg: String?, arg1: Any?, arg2: Any?): Unit = underlyingLogger.trace(msg, arg1, arg2)
+  @Deprecated("Use trace {} instead")
+  public override fun trace(marker: Marker?, msg: String?, arg: Any?): Unit =
+    underlyingLogger.trace(marker?.toSlf4j(), msg, arg)
 
+  @Deprecated("Use trace {} instead")
+  public override fun trace(marker: Marker?, msg: String?, arg1: Any?, arg2: Any?): Unit =
+    underlyingLogger.trace(marker?.toSlf4j(), msg, arg1, arg2)
 
-    @Deprecated("Use trace {} instead")
-    public override fun trace(msg: String?, vararg arguments: Any?): Unit = underlyingLogger.trace(msg, *arguments)
+  @Deprecated("Use trace {} instead")
+  public override fun trace(marker: Marker?, msg: String?, vararg arguments: Any?): Unit =
+    underlyingLogger.trace(marker?.toSlf4j(), msg, *arguments)
 
+  @Deprecated("Use debug {} instead")
+  public override fun debug(msg: String?, arg: Any?): Unit = underlyingLogger.debug(msg, arg)
 
-    @Deprecated("Use trace {} instead")
-    public override fun trace(marker: Marker?, msg: String?, arg: Any?): Unit = underlyingLogger.trace(marker?.toSlf4j(), msg, arg)
+  @Deprecated("Use debug {} instead")
+  public override fun debug(msg: String?, arg1: Any?, arg2: Any?): Unit =
+    underlyingLogger.debug(msg, arg1, arg2)
 
+  @Deprecated("Use debug {} instead")
+  public override fun debug(msg: String?, vararg arguments: Any?): Unit =
+    underlyingLogger.debug(msg, *arguments)
 
-    @Deprecated("Use trace {} instead")
-    public override fun trace(marker: Marker?, msg: String?, arg1: Any?, arg2: Any?): Unit = underlyingLogger.trace(marker?.toSlf4j(), msg, arg1, arg2)
+  @Deprecated("Use debug {} instead")
+  public override fun debug(marker: Marker?, msg: String?, arg: Any?): Unit =
+    underlyingLogger.debug(marker?.toSlf4j(), msg, arg)
 
+  @Deprecated("Use debug {} instead")
+  public override fun debug(marker: Marker?, msg: String?, arg1: Any?, arg2: Any?): Unit =
+    underlyingLogger.debug(marker?.toSlf4j(), msg, arg1, arg2)
 
-    @Deprecated("Use trace {} instead")
-    public override fun trace(marker: Marker?, msg: String?, vararg arguments: Any?): Unit = underlyingLogger.trace(marker?.toSlf4j(), msg, *arguments)
+  @Deprecated("Use debug {} instead")
+  public override fun debug(marker: Marker?, msg: String?, vararg arguments: Any?): Unit =
+    underlyingLogger.debug(marker?.toSlf4j(), msg, *arguments)
 
+  @Deprecated("Use info {} instead")
+  public override fun info(msg: String?, arg: Any?): Unit = underlyingLogger.info(msg, arg)
 
+  @Deprecated("Use info {} instead")
+  public override fun info(msg: String?, arg1: Any?, arg2: Any?): Unit =
+    underlyingLogger.info(msg, arg1, arg2)
 
+  @Deprecated("Use info {} instead")
+  public override fun info(msg: String?, vararg arguments: Any?): Unit =
+    underlyingLogger.info(msg, *arguments)
 
-    @Deprecated("Use debug {} instead") public override fun debug(msg: String?, arg: Any?): Unit = underlyingLogger.debug(msg, arg)
+  @Deprecated("Use info {} instead")
+  public override fun info(marker: Marker?, msg: String?, arg: Any?): Unit =
+    underlyingLogger.info(marker?.toSlf4j(), msg, arg)
 
+  @Deprecated("Use info {} instead")
+  public override fun info(marker: Marker?, msg: String?, arg1: Any?, arg2: Any?): Unit =
+    underlyingLogger.info(marker?.toSlf4j(), msg, arg1, arg2)
 
-    @Deprecated("Use debug {} instead")
-    public override fun debug(msg: String?, arg1: Any?, arg2: Any?): Unit = underlyingLogger.debug(msg, arg1, arg2)
+  @Deprecated("Use info {} instead")
+  public override fun info(marker: Marker?, msg: String?, vararg arguments: Any?): Unit =
+    underlyingLogger.info(marker?.toSlf4j(), msg, *arguments)
 
+  @Deprecated("Use warn {} instead")
+  public override fun warn(msg: String?, arg: Any?): Unit = underlyingLogger.warn(msg, arg)
 
-    @Deprecated("Use debug {} instead")
-    public override fun debug(msg: String?, vararg arguments: Any?): Unit = underlyingLogger.debug(msg, *arguments)
+  @Deprecated("Use warn {} instead")
+  public override fun warn(msg: String?, vararg arguments: Any?): Unit =
+    underlyingLogger.warn(msg, *arguments)
 
+  @Deprecated("Use warn {} instead")
+  public override fun warn(msg: String?, arg1: Any?, arg2: Any?): Unit =
+    underlyingLogger.warn(msg, arg1, arg2)
 
+  @Deprecated("Use warn {} instead")
+  public override fun warn(marker: Marker?, msg: String?, arg: Any?): Unit =
+    underlyingLogger.warn(marker?.toSlf4j(), msg, arg)
 
-    @Deprecated("Use debug {} instead")
-    public override fun debug(marker: Marker?, msg: String?, arg: Any?): Unit = underlyingLogger.debug(marker?.toSlf4j(), msg, arg)
+  public override fun warn(marker: Marker?, msg: String?, arg1: Any?, arg2: Any?): Unit =
+    underlyingLogger.warn(marker?.toSlf4j(), msg, arg1, arg2)
 
+  @Deprecated("Use warn {} instead")
+  public override fun warn(marker: Marker?, msg: String?, vararg arguments: Any?): Unit =
+    underlyingLogger.warn(marker?.toSlf4j(), msg, *arguments)
 
-    @Deprecated("Use debug {} instead")
-    public override fun debug(marker: Marker?, msg: String?, arg1: Any?, arg2: Any?): Unit = underlyingLogger.debug(marker?.toSlf4j(), msg, arg1, arg2)
+  @Deprecated("Use error {} instead")
+  public override fun error(msg: String?, arg: Any?): Unit = underlyingLogger.error(msg, arg)
 
+  @Deprecated("Use error {} instead")
+  public override fun error(msg: String?, arg1: Any?, arg2: Any?): Unit =
+    underlyingLogger.error(msg, arg1, arg2)
 
-    @Deprecated("Use debug {} instead")
-    public override fun debug(marker: Marker?, msg: String?, vararg arguments: Any?): Unit = underlyingLogger.debug(marker?.toSlf4j(), msg, *arguments)
+  @Deprecated("Use error {} instead")
+  public override fun error(msg: String?, vararg arguments: Any?): Unit =
+    underlyingLogger.error(msg, *arguments)
 
+  public override fun error(marker: Marker?, msg: String?, arg: Any?): Unit =
+    underlyingLogger.error(marker?.toSlf4j(), msg, arg)
 
+  @Deprecated("Use error(marker){} instead")
+  public override fun error(marker: Marker?, msg: String?, arg1: Any?, arg2: Any?): Unit =
+    underlyingLogger.error(marker?.toSlf4j(), msg, arg1, arg2)
 
-    @Deprecated("Use info {} instead") public override fun info(msg: String?, arg: Any?): Unit = underlyingLogger.info(msg, arg)
-
-
-    @Deprecated("Use info {} instead")
-    public override fun info(msg: String?, arg1: Any?, arg2: Any?): Unit = underlyingLogger.info(msg, arg1, arg2)
-
-
-    @Deprecated("Use info {} instead")
-    public override fun info(msg: String?, vararg arguments: Any?): Unit = underlyingLogger.info(msg, *arguments)
-
-
-
-
-    @Deprecated("Use info {} instead")
-    public override fun info(marker: Marker?, msg: String?, arg: Any?): Unit = underlyingLogger.info(marker?.toSlf4j(), msg, arg)
-
-
-    @Deprecated("Use info {} instead")
-    public override fun info(marker: Marker?, msg: String?, arg1: Any?, arg2: Any?): Unit = underlyingLogger.info(marker?.toSlf4j(), msg, arg1, arg2)
-
-
-    @Deprecated("Use info {} instead")
-    public override fun info(marker: Marker?, msg: String?, vararg arguments: Any?): Unit = underlyingLogger.info(marker?.toSlf4j(), msg, *arguments)
-
-
-
-    @Deprecated("Use warn {} instead") public override fun warn(msg: String?, arg: Any?): Unit = underlyingLogger.warn(msg, arg)
-
-
-    @Deprecated("Use warn {} instead")
-    public override fun warn(msg: String?, vararg arguments: Any?): Unit = underlyingLogger.warn(msg, *arguments)
-
-
-    @Deprecated("Use warn {} instead")
-    public override fun warn(msg: String?, arg1: Any?, arg2: Any?): Unit = underlyingLogger.warn(msg, arg1, arg2)
-
-
-
-    @Deprecated("Use warn {} instead")
-    public override fun warn(marker: Marker?, msg: String?, arg: Any?): Unit = underlyingLogger.warn(marker?.toSlf4j(), msg, arg)
-
-
-    public override fun warn(marker: Marker?, msg: String?, arg1: Any?, arg2: Any?): Unit = underlyingLogger.warn(marker?.toSlf4j(), msg, arg1, arg2)
-
-
-    @Deprecated("Use warn {} instead")
-    public override fun warn(marker: Marker?, msg: String?, vararg arguments: Any?): Unit = underlyingLogger.warn(marker?.toSlf4j(), msg, *arguments)
-
-
-    @Deprecated("Use error {} instead") public override fun error(msg: String?, arg: Any?): Unit = underlyingLogger.error(msg, arg)
-
-
-    @Deprecated("Use error {} instead")
-    public override fun error(msg: String?, arg1: Any?, arg2: Any?): Unit = underlyingLogger.error(msg, arg1, arg2)
-
-
-    @Deprecated("Use error {} instead")
-    public override fun error(msg: String?, vararg arguments: Any?): Unit = underlyingLogger.error(msg, *arguments)
-
-
-        public override fun error(marker: Marker?, msg: String?, arg: Any?): Unit = underlyingLogger.error(marker?.toSlf4j(), msg, arg)
-
-
-    @Deprecated("Use error(marker){} instead")
-    public override fun error(marker: Marker?, msg: String?, arg1: Any?, arg2: Any?): Unit = underlyingLogger.error(marker?.toSlf4j(), msg, arg1, arg2)
-
-
-    @Deprecated("Use error(marker){} instead")
-    public override fun error(marker: Marker?, msg: String?, vararg arguments: Any?): Unit = underlyingLogger.error(marker?.toSlf4j(), msg, *arguments)
-
+  @Deprecated("Use error(marker){} instead")
+  public override fun error(marker: Marker?, msg: String?, vararg arguments: Any?): Unit =
+    underlyingLogger.error(marker?.toSlf4j(), msg, *arguments)
 }
