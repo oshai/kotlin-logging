241d7b1c6f477e52e8285eb5b2a32e01ac597a31
oshai <ohadshai@gmail.com>
2023-08-09 16:39:19 +0300

    add deprecated fields

--- src/jsMain/kotlin/io/github/oshai/kotlinlogging/KotlinLoggingConfiguration.kt
+++ src/jsMain/kotlin/io/github/oshai/kotlinlogging/KotlinLoggingConfiguration.kt
@@ -4,4 +4,17 @@ public actual object KotlinLoggingConfiguration {
   public actual var logLevel: Level = Level.INFO
   public actual var formatter: Formatter = DefaultMessageFormatter(includePrefix = true)
   public actual var appender: Appender = ConsoleOutputAppender()
+
+  @Deprecated("Use appender instead", ReplaceWith("appender"))
+  public var APPENDER: Appender
+    get() = appender
+    set(value) {
+      appender = value
+    }
+  @Deprecated("Use logLevel instead", ReplaceWith("logLevel"))
+  public var LOG_LEVEL: Level
+    get() = logLevel
+    set(value) {
+      logLevel = value
+    }
 }
