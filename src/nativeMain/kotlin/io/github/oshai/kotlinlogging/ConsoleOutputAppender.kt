24baa34689d714fe38176749e7eea50a362ce4a3
oshai <ohadshai@gmail.com>
2023-12-18 17:33:00 +0200

    fix - protect from uncontrolled format string
    Protect from string format attack:
    https://owasp.org/www-community/attacks/Format_string_attack

--- src/nativeMain/kotlin/io/github/oshai/kotlinlogging/ConsoleOutputAppender.kt
+++ src/nativeMain/kotlin/io/github/oshai/kotlinlogging/ConsoleOutputAppender.kt
@@ -6,7 +6,7 @@ import platform.posix.stderr
 public object ConsoleOutputAppender : FormattingAppender() {
   override fun logFormattedMessage(loggingEvent: KLoggingEvent, formattedMessage: Any?) {
     if (loggingEvent.level == Level.ERROR) {
-      fprintf(stderr, "$formattedMessage\n")
+      fprintf(stderr, "%s\n", formattedMessage.toString())
     } else {
       println(formattedMessage)
     }
