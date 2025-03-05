d2b0fc72c1bcf5c88a878be9bfb090b2a01445a3
Neeme Praks <neeme.praks@eesti.ee>
2025-03-05 09:47:09 +0200

    Simplify LogbackLoggerFactory-related code

--- src/jvmMain/kotlin/io/github/oshai/kotlinlogging/internal/KLoggerFactory.kt
+++ src/jvmMain/kotlin/io/github/oshai/kotlinlogging/internal/KLoggerFactory.kt
@@ -4,8 +4,6 @@ import io.github.oshai.kotlinlogging.KLogger
 import io.github.oshai.kotlinlogging.jul.internal.JulLoggerFactory
 import io.github.oshai.kotlinlogging.logback.internal.LogbackLoggerFactory
 import io.github.oshai.kotlinlogging.slf4j.internal.Slf4jLoggerFactory
-import org.slf4j.ILoggerFactory
-import org.slf4j.LoggerFactory
 
 /** factory methods to obtain a [KLogger] */
 internal actual object KLoggerFactory {
@@ -14,16 +12,10 @@ internal actual object KLoggerFactory {
   internal actual fun logger(name: String): KLogger {
     if (System.getProperty("kotlin-logging-to-jul") != null) {
       return JulLoggerFactory.wrapJLogger(JulLoggerFactory.jLogger(name))
+    } else if (System.getProperty("kotlin-logging-to-logback") == "true") {
+      return LogbackLoggerFactory.logbackLogger(name)
     }
-    // Try Logback and if that fails then default to plain SLF4J
-    return if (LoggerFactory.getILoggerFactory().isLogback()) {
-      LogbackLoggerFactory.logbackLogger(name)
-    } else {
-      Slf4jLoggerFactory.wrapJLogger(Slf4jLoggerFactory.jLogger(name))
-    }
+    // default to SLF4J
+    return Slf4jLoggerFactory.wrapJLogger(Slf4jLoggerFactory.jLogger(name))
   }
 }
-
-private fun ILoggerFactory.isLogback() =
-  System.getProperty("kotlin-logging-to-logback") == "true" &&
-    this.javaClass.name.startsWith("ch.qos.logback.classic")
