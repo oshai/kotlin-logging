d79a82fdd2944605f54ac74e56e9b72a44d2ed5a
Ohad Shai <oshai@google.com>
2025-08-02 01:58:22 +0300

    add one test

---
+++ src/nativeTest/kotlin/io/github/oshai/kotlinlogging/ClassLoggerNativeTest.kt
@@ -0,0 +1,15 @@
+package io.github.oshai.kotlinlogging
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ClassLoggerNativeTest {
+
+  private val logger = KotlinLogging.logger {}
+
+  @Test
+  fun loggerNameIsCorrectlyInferredFromClass() {
+    println("Asserting logger name from class: Expected 'io.github.oshai.kotlinlogging.ClassLoggerNativeTest', was '${logger.name}'")
+    assertEquals("io.github.oshai.kotlinlogging.ClassLoggerNativeTest", logger.name)
+  }
+}
